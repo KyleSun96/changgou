0eb7a5446e7f3fc72a6ba9130732381262332231
KyleSun <swy0907163@163.com>
2020-05-29 19:31:33 +0800

    添加公共异常处理

--- changgou_parent/changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/handler/BaseExceptionHandler.java
+++ changgou_parent/changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/handler/BaseExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.changgou.goods.controller;
+package com.changgou.goods.handler;
 
 import com.changgou.entity.Result;
 import com.changgou.entity.StatusCode;
@@ -7,15 +7,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 统一异常处理类
- */
-@ControllerAdvice
+ * @Program: ChangGou
+ * @ClassName: BaseExceptionHandler
+ * @Description: 公共异常处理类
+ * @Author: KyleSun
+ **/
+@ControllerAdvice   // 声明该类是个增强类
 public class BaseExceptionHandler {
 
-    @ExceptionHandler(value = Exception.class)
     @ResponseBody
+    @ExceptionHandler(value = Exception.class)  // 声明对哪个异常进行处理
     public Result error(Exception e) {
         e.printStackTrace();
-        return new Result(false, StatusCode.ERROR, "执行出错");
+        return new Result(false, StatusCode.ERROR, "当前系统繁忙，请稍后重试！");
     }
+
 }
