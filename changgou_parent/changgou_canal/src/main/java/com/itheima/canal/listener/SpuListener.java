071dc979f0187db5e4f0754cf941fb6db0d4fb94
KyleSun <swy0907163@163.com>
2020-06-04 20:12:43 +0800

    feat: 创建商品上架rabbitMQ和skuListener监听类

--- changgou_parent/changgou_canal/src/main/java/com/itheima/canal/listener/SpuListener.java
+++ changgou_parent/changgou_canal/src/main/java/com/itheima/canal/listener/SpuListener.java
@@ -1,6 +1,7 @@
 package com.itheima.canal.listener;
 
 import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.itheima.canal.config.RabbitMQConfig;
 import com.xpand.starter.canal.annotation.CanalEventListener;
 import com.xpand.starter.canal.annotation.ListenPoint;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -10,8 +11,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author ZJ
- */
+ * @Program: ChangGou
+ * @ClassName: SpuListener
+ * @Description: spu监听类
+ * @Author: KyleSun
+ **/
+// 声明当前的类是canal的监听类
 @CanalEventListener
 public class SpuListener {
 
@@ -19,30 +24,29 @@ public class SpuListener {
     private RabbitTemplate rabbitTemplate;
 
     /**
-     * spu 表更新
-     *
-     * @param eventType
-     * @param rowData
+     * @description: //TODO spu表更新
+     * @param: [eventType, rowData] 【对数据库操作的事件类型，对数据库操作的具体数据】
+     * @return: void
      */
     @ListenPoint(schema = "changgou_goods", table = {"tb_spu"}, eventType = CanalEntry.EventType.UPDATE)
     public void spuUp(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+
         System.err.println("tb_spu表数据发生变化");
 
-        //修改前数据
-        Map oldMap = new HashMap<>();
+        // 获取修改前数据，并转换为 map
+        Map<String, String> oldData = new HashMap<>();
         for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
-            oldMap.put(column.getName(), column.getValue());
+            oldData.put(column.getName(), column.getValue());
         }
 
-        //修改后数据
-        Map newMap = new HashMap<>();
-        for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
-            newMap.put(column.getName(), column.getValue());
-        }
+        // 获取修改后数据，并转换为 map (λ表达式写法)
+        Map<String, String> newData = new HashMap<>();
+        rowData.getAfterColumnsList().forEach(column -> newData.put(column.getName(), column.getValue()));
 
-        //is_marketable  由0改为1表示上架
-        if ("0".equals(oldMap.get("is_marketable")) && "1".equals(newMap.get("is_marketable"))) {
-            rabbitTemplate.convertAndSend("goods_update_exchange", "", newMap.get("id")); //发送到mq商品上架交换器上
+        // is_marketable --> 由0改为1表示最新上架的商品
+        if ("0".equals(oldData.get("is_marketable")) && "1".equals(newData.get("is_marketable"))) {
+            // 将商品的 spuId 发送到 mq
+            rabbitTemplate.convertAndSend(RabbitMQConfig.GOODS_UP_EXCHANGE, "", newData.get("id"));
         }
     }
 }
