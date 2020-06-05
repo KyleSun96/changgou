63ad3e7601aa50990b4776712f60abb838e9799f
KyleSun <swy0907163@163.com>
2020-06-05 09:11:36 +0800

    feat: 商品搜索功能（未添加高亮）

--- changgou_parent/changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
+++ changgou_parent/changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
@@ -17,15 +17,31 @@ public class SearchController {
     @Autowired
     private SearchService searchService;
 
+
+    /**
+     * @description: //TODO 商品搜索
+     * @param: [searchMap]
+     * @return: java.util.Map
+     */
     @GetMapping
     public Map search(@RequestParam Map<String, String> searchMap) {
-        //特殊符号处理
+        /*
+            特殊符号处理
+                商品的规格中可能含有特殊符号，如 = + 等，无法在URL路径中直接传递（get请求）
+                因此需要将 '+' 转为 '%2B'
+         */
         this.handleSearchMap(searchMap);
-        Map searchResult = searchService.search(searchMap);
-        return searchResult;
+        return searchService.search(searchMap);
     }
 
+
+    /**
+     * @description: //TODO 特殊符号处理
+     * @param: [searchMap]
+     * @return: void
+     */
     private void handleSearchMap(Map<String, String> searchMap) {
+        // 获取map中的每一个键值对
         Set<Map.Entry<String, String>> entries = searchMap.entrySet();
         for (Map.Entry<String, String> entry : entries) {
             if (entry.getKey().startsWith("spec_")) {
--- changgou_parent/changgou_service/changgou_service_search/src/main/java/com/changgou/search/service/SearchService.java
+++ changgou_parent/changgou_service/changgou_service_search/src/main/java/com/changgou/search/service/SearchService.java
@@ -4,6 +4,12 @@ import java.util.Map;
 
 public interface SearchService {
 
-    // 按照查询条件进行数据查询
+    /*
+        按照查询条件进行数据查询
+            返回值使用 map 类型是因为页面不仅需要返回的查询数据，还有分页的数据存在
+            参数使用 map 类型是因为商品搜索不仅只使用关键字查询，可能还需要品牌、规格等参数查询
+        因此为了程序后期的可扩展性，我们此时使用 map 类型
+     */
     Map search(Map<String, String> searchMap);
+
 }
