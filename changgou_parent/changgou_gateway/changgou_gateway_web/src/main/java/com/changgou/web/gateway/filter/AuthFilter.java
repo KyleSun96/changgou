141452175a15e24022492c54ee22d0db511565c7
KyleSun <swy0907163@163.com>
2020-06-11 01:46:08 +0800

    feat: 未登录时登录跳转

--- changgou_parent/changgou_gateway/changgou_gateway_web/src/main/java/com/changgou/web/gateway/filter/AuthFilter.java
+++ changgou_parent/changgou_gateway/changgou_gateway_web/src/main/java/com/changgou/web/gateway/filter/AuthFilter.java
@@ -22,6 +22,9 @@ import reactor.core.publisher.Mono;
 @Component
 public class AuthFilter implements GlobalFilter, Ordered {
 
+    // 设置未登录时的登录页面跳转路径
+    private static final String LOGIN_URL = "http://localhost:8001/api/oauth/toLogin";
+
     @Autowired
     private AuthService authService;
 
@@ -42,21 +45,41 @@ public class AuthFilter implements GlobalFilter, Ordered {
         String jti = authService.getJtiFromCookie(request);
         if (StringUtils.isEmpty(jti)) {
             // 拒绝访问
-            response.setStatusCode(HttpStatus.UNAUTHORIZED);
-            return response.setComplete();
+            /*response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();*/
+            // 跳转到登录界面
+            return this.toLoginPage(LOGIN_URL, exchange);
+
         }
 
         // 3.从redis中获取jwt的值，如果该值不存在，拒绝本次访问
         String jwt = authService.getJwtFromRedis(jti);
         if (StringUtils.isEmpty(jwt)) {
             // 拒绝访问
-            response.setStatusCode(HttpStatus.UNAUTHORIZED);
-            return response.setComplete();
+            /*response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();*/
+            // 跳转到登录界面
+            return this.toLoginPage(LOGIN_URL, exchange);
         }
 
         // 4.对当前的请求对象进行增强，让它会携带令牌的信息
         request.mutate().header("Authorization", "Bearer " + jwt);
         return chain.filter(exchange);
+    }
+
+
+    /**
+     * @description: //TODO 跳转到登录界面
+     * @param: [loginUrl, exchange]
+     * @return: reactor.core.publisher.Mono<java.lang.Void>
+     */
+    private Mono<Void> toLoginPage(String loginUrl, ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        // 设置状态码302 跳转
+        response.setStatusCode(HttpStatus.SEE_OTHER);
+        // header中添加跳转地址
+        response.getHeaders().set("Location", loginUrl);
+        return response.setComplete();
     }
 
     @Override
