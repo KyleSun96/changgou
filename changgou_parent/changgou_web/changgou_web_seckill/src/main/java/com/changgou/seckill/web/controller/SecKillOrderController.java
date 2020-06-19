4a9b01d479ee7d9300f2d4c8ab159a649c105840
KyleSun <swy0907163@163.com>
2020-06-20 02:50:37 +0800

    feat: 实现秒杀下单接口隐藏

--- changgou_parent/changgou_web/changgou_web_seckill/src/main/java/com/changgou/seckill/web/controller/SecKillOrderController.java
+++ changgou_parent/changgou_web/changgou_web_seckill/src/main/java/com/changgou/seckill/web/controller/SecKillOrderController.java
@@ -1,11 +1,19 @@
 package com.changgou.seckill.web.controller;
 
 import com.changgou.entity.Result;
+import com.changgou.entity.StatusCode;
 import com.changgou.seckill.feign.SecKillOrderFeign;
+import com.changgou.seckill.web.util.CookieUtil;
+import com.changgou.util.RandomUtil;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Program: ChangGou
@@ -20,14 +28,62 @@ public class SecKillOrderController {
     @Autowired
     private SecKillOrderFeign secKillOrderFeign;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     /**
      * @description: //TODO 秒杀异步下单
      * @param: [time, id]
      * @return: com.changgou.entity.Result
      */
     @RequestMapping("/add")
-    public Result add(@RequestParam("time") String time, @RequestParam("id") Long id) {
+    public Result add(@RequestParam("time") String time, @RequestParam("id") Long id, @RequestParam("random") String random) {
+
+        // 验证随机数是否正确，实现秒杀下单接口隐藏
+        String cookie = this.readCookie();
+        String redisRandomCode = (String) redisTemplate.opsForValue().get("randomcode_" + cookie);
+
+        if (StringUtils.isEmpty(redisRandomCode)) {
+            return new Result(false, StatusCode.ERROR, "下单失败");
+        }
+        if (!random.equals(redisRandomCode)) {
+            return new Result(false, StatusCode.ERROR, "下单失败");
+        }
+
         return secKillOrderFeign.add(time, id);
     }
 
+
+    /**
+     * @description: //TODO 获取一个随机数
+     * @param: []
+     * @return: java.lang.String
+     */
+    @GetMapping("/getToken")
+    @ResponseBody
+    public String getToken() {
+
+        String randomString = RandomUtil.getRandomString();
+
+        String cookieValue = this.readCookie();
+
+        redisTemplate.opsForValue().set("randomcode_" + cookieValue, randomString, 5, TimeUnit.SECONDS);
+
+        return randomString;
+    }
+
+
+    /**
+     * @description: //TODO 从cookie中获取jti
+     * @param: []
+     * @return: java.lang.String
+     */
+    private String readCookie() {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+        String jti = CookieUtil.readCookie(request, "uid").get("uid");
+
+        return jti;
+    }
 }
---
+++ changgou_parent/changgou_web/changgou_web_seckill/src/main/java/com/changgou/seckill/web/util/CookieUtil.java
@@ -0,0 +1,59 @@
+package com.changgou.seckill.web.util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 操作cookie工具类
+ */
+public class CookieUtil {
+
+
+    /**
+     * 添加cookie
+     *
+     * @param response
+     * @param domain        设置cookie域名
+     * @param path          设置cookie路径
+     * @param name          设置cookie名称
+     * @param value         设置cookie的值，即jti
+     * @param maxAge        设置cookie生命周期，以秒为单位
+     * @param httpOnly
+     */
+    public static void addCookie(HttpServletResponse response, String domain, String path, String name,
+                                 String value, int maxAge, boolean httpOnly) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setDomain(domain);
+        cookie.setPath(path);
+        cookie.setMaxAge(maxAge);
+        cookie.setHttpOnly(httpOnly);
+        response.addCookie(cookie);
+    }
+
+
+    /**
+     * 根据cookie名称读取cookie
+     *
+     * @param request
+     * @return map<cookieName, cookieValue>
+     */
+    public static Map<String, String> readCookie(HttpServletRequest request, String... cookieNames) {
+        Map<String, String> cookieMap = new HashMap<String, String>();
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                String cookieName = cookie.getName();
+                String cookieValue = cookie.getValue();
+                for (int i = 0; i < cookieNames.length; i++) {
+                    if (cookieNames[i].equals(cookieName)) {
+                        cookieMap.put(cookieName, cookieValue);
+                    }
+                }
+            }
+        }
+        return cookieMap;
+    }
+}
